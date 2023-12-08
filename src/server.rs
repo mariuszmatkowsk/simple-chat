62c9d3c228f6887ae22cb3af3c3be2c79181d216
mariusz.matkowski <mariusz.matkowski@yahoo.com>
2023-12-08 10:57:54 +0100

    Added basic functionality for server. Now we can send messages to others

--- src/server.rs
+++ src/server.rs
@@ -10,20 +10,41 @@ type Result<T> = result::Result<T, ()>;
 enum Message {
     ClientConnected { address: SocketAddr, stream: Arc<TcpStream> },
     ClientDisconnected { address: SocketAddr },
-    NewMessage { author: SocketAddr },
+    NewMessage { author: SocketAddr, message: String },
 }
 
 fn server(receiver: Receiver<Message>) -> Result<()> {
-    let clients: HashMap<SocketAddr, Arc<TcpStream>> = HashMap::new();
+    let mut clients: HashMap<SocketAddr, Arc<TcpStream>> = HashMap::new();
     loop {
         let message = receiver.recv().map_err(|err| {
             eprintln!("ERROR: connection hang up: {err}");
         })?;
 
         match message {
-            Message::ClientConnected { address, stream }=> todo!(),
-            Message::ClientDisconnected { address } => todo!(),
-            Message::NewMessage { author }=> todo!(),
+            Message::ClientConnected { address, stream } => {
+                println!("INFO: client {address} is now connected.");
+                stream.as_ref().write(b"You are connected to awesome server.").map_err(|err| {
+                    eprintln!("ERROR: could not send message to {address}: {err}");
+                })?;
+                clients.insert(address, stream);
+            },
+            Message::ClientDisconnected { address } => {
+                if let Some(stream) = clients.remove(&address) {
+                    println!("INFO: client {address} was disconnected.");
+                    stream.as_ref().write(b"You are disconnected.").map_err(|err| {
+                        eprintln!("ERROR: could not send message to {address}: {err}");
+                    })?;
+                }
+            },
+            Message::NewMessage { author, message } => {
+                for (addr, stream) in clients.iter() {
+                    if *addr != author {
+                        stream.as_ref().write(message.as_bytes()).map_err(|err| {
+                            eprintln!("ERROR: could not send message to {addr}: {err}");
+                        })?;  
+                    }
+                }
+            },
         }
     }
 }
@@ -38,7 +59,7 @@ fn client(stream: Arc<TcpStream>, sender: Sender<Message>) -> Result<()> {
     })?;
 
     sender.send(Message::ClientConnected { address, stream: stream.clone() }).map_err(|err| {
-        eprintln!("ERROR: could not send message to server: {err}");
+        eprintln!("ERROR: could not send message ClientConnected to server: {err}");
     })?;
    
     let mut buff = [0; 64];
@@ -48,14 +69,21 @@ fn client(stream: Arc<TcpStream>, sender: Sender<Message>) -> Result<()> {
         })?;
 
         if n == 0 {
-            // Todo: Connection closed by client
+            sender.send(Message::ClientDisconnected { address }).map_err(|err| {
+                eprintln!("ERROR: could not send message ClientDisconnected to server: {err}");
+            })?;
         } else {
-            // Todo: send message to server
+            let mut mesg = String::new();
+            for c in buff.iter(){
+                if *c >= 32 {   // This removes escape keys
+                    mesg.push(*c as char);
+                }
+            }
+            sender.send(Message::NewMessage { author: address, message: mesg }).map_err(|err| {
+                eprintln!("ERROR: could not send Message::NewMessage to server: {err}");
+            })?;
         }
-
-        
     }
-
 }
 
 fn main() -> Result<()> {
@@ -70,7 +98,7 @@ fn main() -> Result<()> {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 let sender = sender.clone();
                 let stream = Arc::new(stream);
                 std::thread::spawn(move || client(stream, sender));
